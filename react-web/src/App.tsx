c2faae5cee2a21c3a25bf038ef2baba992d3b0fc
Sandushan8 <dewpurai@gmail.com>
2024-07-06 16:51:42 +0530

    Initial Commit

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Experience } from "./screen/experience/Experience";
+import { Layout } from "./screen/layout/Layout";
+import { Myself } from "./screen/mySelf/Myself";
+import { Skills } from "./screen/skills/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-roboto">
+      <Layout>
+        <Myself />
+        <Skills />
+        <Experience />
+      </Layout>
     </div>
   );
 }
---
+++ react-web/src/screen/experience/Experience.tsx
@@ -0,0 +1,7 @@
+export const Experience = () => {
+  return (
+    <div className="text-white ml-16 mr-16 h-screen">
+      <h1 className="text-5xl font-bold text-greetBlue">Experience</h1>
+    </div>
+  );
+};
